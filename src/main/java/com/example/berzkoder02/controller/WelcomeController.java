46a6c4020744fada3de907459279812154c37cce
budi sentosa <budi@avasoft.co>
2022-12-14 15:18:15 +0700

    hello + actuator + mysql connection

---
+++ src/main/java/com/example/berzkoder02/controller/WelcomeController.java
@@ -0,0 +1,15 @@
+package com.example.berzkoder02.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/welcome")
+public class WelcomeController {
+
+    @GetMapping
+    public String welcome() {
+        return "Welcome";
+    }
+}
