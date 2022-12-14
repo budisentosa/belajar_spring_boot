485d78d27d7a41d60773c97e5a5f6cfc9459e6eb
budi sentosa <budi@avasoft.co>
2022-12-14 09:06:02 +0700

    init

---
+++ src/main/java/com/example/berzkoder02/Berzkoder02Application.java
@@ -0,0 +1,13 @@
+package com.example.berzkoder02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Berzkoder02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Berzkoder02Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/berzkoder02/Berzkoder02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.berzkoder02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Berzkoder02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
