51747f32365fe1ef5b0488503ca2bdd345034b32
budi sentosa <budi@avasoft.co>
2022-12-26 14:33:39 +0700

    tidak konsisten , record 1 dan 2

--- src/main/java/com/example/berzkoder02/models/entities/Supplier.java
+++ src/main/java/com/example/berzkoder02/models/entities/Supplier.java
@@ -1,8 +1,6 @@
 package com.example.berzkoder02.models.entities;
 
 import java.io.Serializable;
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -18,9 +16,6 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@JsonIdentityInfo(
-    generator = ObjectIdGenerators.PropertyGenerator.class, property = "id"
-)
 public class Supplier implements Serializable{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
